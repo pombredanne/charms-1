3a69d194f9534c0378ee60944a562f7d845df91c
Flavia Missi <flaviamissi@gmail.com>
2012-02-29 16:33:17 -0300

    Exporting environment variables on gunicorn start script

---
+++ oneiric/django/utils/environment.sh
@@ -0,0 +1,9 @@
+database=`relation-get database`
+user=`relation-get user`
+password=`relation-get password`
+host=`relation-get private-address`
+
+export DB_NAME=$database
+export DB_USER=$user
+export DB_PASSWORD=$password
+export DB_HOST=$host
