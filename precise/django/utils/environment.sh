7c6430047652f90ff95884f284f8e8de786d2d75
Flavia Missi <flaviamissi@gmail.com>
2012-10-03 15:54:58 -0300

    precise: removed gunicorn and django charms, use python with a Procfile instead

--- precise/django/utils/environment.sh
+++
@@ -1,9 +0,0 @@
-database=`relation-get database`
-user=`relation-get user`
-password=`relation-get password`
-host=`relation-get private-address`
-
-export DB_NAME=$database
-export DB_USER=$user
-export DB_PASSWORD=$password
-export DB_HOST=$host
