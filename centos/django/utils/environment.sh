96e7b76ccf8a2210acde9688463bcf0df2424bab
Francisco Souza <fss@corp.globo.com>
2013-06-19 14:37:46 -0300

    centos: delete directory
    It's outdated and not maintained.

--- centos/django/utils/environment.sh
+++
@@ -1,6 +0,0 @@
-DB_NAME=`relation-get database`
-DB_USER=`relation-get user`
-DB_PASSWORD=`relation-get password`
-DB_HOST=`relation-get private-address`
-
-export DB_NAME DB_USER DB_PASSWORD DB_HOST
